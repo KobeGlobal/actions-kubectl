3c0f08416ccfeaa81549256074eaed471d39f608
Noi Vu <noivuduc@gmail.com>
2020-05-18 15:17:33 +0800

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,9 @@
 } >> kubeconfig
 
 export KUBECONFIG=kubeconfig
-
+sed -i.bak 's#{replicas}#${ REPLICA }#' $1
+sed -i.bak 's#{container_image}#${ IMAGE }#' $1
+sed -i.bak 's#{secret_name}#${ SECRET_NAME }#' $1
 result="$(kubectl $1)"
 
 status=$?
