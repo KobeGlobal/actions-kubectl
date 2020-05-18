7886c29a5aa3d1f8df2bf5edadbf9ce9a2b5a775
Noi Vu <noivuduc@gmail.com>
2020-05-18 14:34:09 +0800

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh -l
-echo `cat config.txt` | base64 -d > kubeconfig
+echo `apiVersion: v1
+kind: Config
+clusters:
+- name: "ims"
+  cluster:
+    server: "https://rancher.getkobe.com/k8s/clusters/c-6qkj6"
+
+users:
+- name: "ims"
+  user:
+    token: "kubeconfig-u-95mpm:mr4drptcm4fqbd8m6g769wnq2gfbmxgl4jv48nx6xhsqdjrqpwsh4l"
+
+contexts:
+- name: "ims"
+  context:
+    user: "ims"
+    cluster: "ims"
+
+current-context: "ims"` | base64 -d > kubeconfig
 
 export KUBECONFIG=kubeconfig
 
